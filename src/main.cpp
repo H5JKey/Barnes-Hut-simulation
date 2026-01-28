d445e3bebb27082d9e55b4c465717a09a3e8921c
Ivan <ivanbelousoff96@gmail.com>
2026-01-28 23:06:46 +0300

    add colors for particles

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,19 @@
 #include <iostream>
 
 enum {
-    PARTICLES_NUM = 3000
+    PARTICLES_NUM = 10000
 };
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 800), "Galaxy Simulation");
     
     PhysicsEngine physics;
-    BarnesHutCalculator forceCalculator(window.getSize(), &physics, 0.5f);
+    sf::View view;
+    view.setSize(10000,10000);
+    view.setCenter(view.getSize()/2.f);
+    BarnesHutCalculator forceCalculator(static_cast<sf::Vector2u>(view.getSize()), &physics, 0.5f);
     
-    ParticleSystem particles(PARTICLES_NUM, window.getSize());
+    ParticleSystem particles(PARTICLES_NUM, static_cast<sf::Vector2u>(view.getSize()));
     std::vector<sf::Vector2f> forces;
 
     bool showTree = false;
@@ -26,6 +29,7 @@ int main() {
     sf::Clock clock;
     sf::Time av = sf::Time::Zero;
     long long cnt = 0;
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -51,12 +55,14 @@ int main() {
         sf::VertexArray particleBodies(sf::Points, particles.getCount());
         for (int i=0; i < particles.getCount(); i++) {
             particleBodies[i].position = particles.getPosition(i);
-            particleBodies[i].color = sf::Color::White;
+            particleBodies[i].color = particles.getColor(i);
         }
+        window.setView(view);
         window.draw(particleBodies);
         if (showTree)
             forceCalculator.getQuadTree()->draw(window);
         window.display();
+        //window.setView(window.getDefaultView());
     }
     std::cout<<"AVERAGE FRAME TIME: "<<av.asMilliseconds()/cnt<<'\n';
     
