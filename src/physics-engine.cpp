047d71e4b3cdbaa961b18e0119bb57440a15a9d5
H5JKey <115498581+H5JKey@users.noreply.github.com>
2026-05-27 00:59:30 +0300

    Update physics-engine.cpp
    Increase softening param from 0.1 to 100 to improve stability

--- src/physics-engine.cpp
+++ src/physics-engine.cpp
@@ -3,7 +3,7 @@
 void PhysicsEngine::calculateForce(float mass1, float position1_x, float position1_y, float mass2, float position2_x, float position2_y, float& force_x, float& force_y) const noexcept {
     float direction_x = position2_x - position1_x;
     float direction_y = position2_y - position1_y;
-    float inv_len = Utils::fastInvSqrt(Utils::computeSquaredLength(direction_x, direction_y)+0.1f);
+    float inv_len = Utils::fastInvSqrt(Utils::computeSquaredLength(direction_x, direction_y)+100.f);
     float magnitude = mass1 * mass2 * G * inv_len * inv_len * inv_len;
     direction_x *= magnitude;
     direction_y *= magnitude;
